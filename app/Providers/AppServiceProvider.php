1483e5b31277df6ede21aa584550e4f5522e5cc2
Jonatan Geovanni Grajales Díaz <jhongeo@jhongeos-iMac.local>
2017-06-27 15:57:57 -0500

    fixed function boot on AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
 
     /**
@@ -26,9 +26,4 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-function boot()
-{ Schema::defaultStringLength(191);
-  
-}
-
 }
